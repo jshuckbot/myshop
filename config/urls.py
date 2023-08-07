64d0e39e597ebee1be53df4d7ccbf6d3a32801c3
jshuckbot <gorka_ivan@mail.ru>
2023-08-07 13:19:33 +0700

    created tempates catalogs

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("shopapp/", include("shopapp.urls", namespace="shopapp")),
+    path("", include("shopapp.urls", namespace="shopapp")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
