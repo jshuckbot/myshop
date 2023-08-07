001255122d443ee02ba0295aee56bbc14a9dc448
jshuckbot <gorka_ivan@mail.ru>
2023-08-07 10:38:24 +0700

    added models in admin-site

--- shopapp/admin.py
+++ shopapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from shopapp import models as shopapp_models
+
+
+@admin.register(shopapp_models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug"]
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(shopapp_models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug", "price", "available", "created", "updated"]
+    list_filter = ["available", "created", "updated"]
+    list_editable = ["price", "available"]
+    prepopulated_fields = {"slug": ("name",)}
