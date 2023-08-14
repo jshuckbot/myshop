5e93de03a7b16764cfe44897d7c147c1b0125b65
Ivan <93859862+jshuckbot@users.noreply.github.com>
2023-08-14 18:03:08 +0700

    Shopapp (#1)
    * added app shop

---
+++ shopapp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import get_object_or_404, render
+
+from shopapp import models as shopapp_models
+
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = shopapp_models.Category.objects.all()
+    products = shopapp_models.Product.objects.filter(available=True)
+
+    if category_slug:
+        category = get_object_or_404(shopapp_models.Category, slug=category_slug)
+        products = products.filter(category=category)
+
+    return render(
+        request, "shopapp/product/list.html", {"category": category, "categories": categories, "products": products}
+    )
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(shopapp_models.Product, id=id, slug=slug, available=True)
+    return render(request, "shopapp/product/detail.html", {"product": product})
