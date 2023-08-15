a5b8c5d16e2dc4a37f2b5dea662b43b1db9e13eb
jshuckbot <gorka_ivan@mail.ru>
2023-08-15 08:35:54 +0700

    added view cart_add

--- cartapp/views.py
+++ cartapp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+from cartapp.cart import Cart
+from cartapp.forms import CartAddProductForm
+from shopapp import models as shopapp_models
+
+
+@require_POST
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(shopapp_models.Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd["quantity"], override_quantity=cd["override_quantity"])
+    return redirect("cartapp:cart_detail")
