b2d2060b58b9d60bfa1f54432ed70ef559c2b85c
jshuckbot <gorka_ivan@mail.ru>
2023-08-15 08:42:02 +0700

    added views are cart_remove, cart_detail

--- cartapp/views.py
+++ cartapp/views.py
@@ -15,3 +15,16 @@ def cart_add(request, product_id):
         cd = form.cleaned_data
         cart.add(product=product, quantity=cd["quantity"], override_quantity=cd["override_quantity"])
     return redirect("cartapp:cart_detail")
+
+
+@require_POST
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(shopapp_models.Product, id=product_id)
+    cart.remove(product)
+    return redirect("cartapp:cart_detail")
+
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, "cartapp/detail.html", {"cart": cart})
