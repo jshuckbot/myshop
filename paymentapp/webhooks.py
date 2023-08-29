ee8ad66222aae837fc3ed62354045161418e144c
jshuckbot <gorka_ivan@mail.ru>
2023-08-29 12:07:08 +0700

    added webhook for Stripe

---
+++ paymentapp/webhooks.py
@@ -0,0 +1,38 @@
+import stripe
+from django.conf import settings
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from orderapp import models as orderapp_models
+
+
+@csrf_exempt
+def stripe_webkook(request):
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+    
+    try:
+        event = stripe.Webhook.construct_event(
+            payload,
+            sig_header,
+            settings.STRIPE_WEBHOOK_SECRET,
+        )
+    except ValueError as e:
+        # Недопустимая полезная нагрузка
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Недопустимая подпись
+        return HttpResponse(status=400)
+    
+    if event.type == 'checkout.session.completed':
+        session = event.data.object
+        if session.mode == 'payment' and session.payment_status == 'paid':
+            try:
+                order = orderapp_models.Order.objects.get(id=session.client_reference_id)
+            except orderapp_models.Order.DoesNotExist:
+                return HttpResponse(status=404)
+            
+            order.paid = True
+            order.save()
+    
+    return HttpResponse(status=200)
