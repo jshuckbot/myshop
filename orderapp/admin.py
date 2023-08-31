ea0459143dd7c54aec9f2313a23bfef2cc922e48
jshuckbot <gorka_ivan@mail.ru>
2023-08-31 16:02:18 +0700

    save orders in csv file

--- orderapp/admin.py
+++ orderapp/admin.py
@@ -1,4 +1,8 @@
+import csv
+import datetime
+
 from django.contrib import admin
+from django.http import HttpResponse
 from django.utils.safestring import mark_safe
 
 from orderapp import models as orderapp_models
@@ -12,7 +16,31 @@ def order_stripe_payment(obj):
     return ""
 
 
-order_stripe_payment.short_description = "Stripe pament"
+order_stripe_payment.short_description = "Stripe payment"
+
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = f"attachment; filename={opts.verbose_name}.csv"
+    response = HttpResponse(content_type="text/csv")
+    response["Content-Disposition"] = content_disposition
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    writer.writerow(field.verbose_name for field in fields)
+
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime("%d/%m/%Y")
+            data_row.append(value)
+        writer.writerow(data_row)
+
+    return response
+
+
+export_to_csv.short_description = "Export to CSV"
 
 
 class OrderItemInline(admin.TabularInline):
@@ -37,3 +65,4 @@ class OrderAdmin(admin.ModelAdmin):
     ]
     list_filter = ["paid", "created", "updated"]
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
